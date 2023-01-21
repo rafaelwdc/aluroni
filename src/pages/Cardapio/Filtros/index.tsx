2b47026b7061b8b549266504e1b979605efd2aae
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-21 02:14:02 -0300

    Fazendo os filtros funcionares// React com TypeScript => React: lidando com arquivos estáticos

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,24 +1,35 @@
+import classNames from 'classnames';
 import filtros from './filtros.json';
 import styles from './Filtros.module.scss';
 
 type IOpcao = typeof filtros[0];
 
+interface Props {
+    filtro: number | null;
+    setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+}
 
-export default function Filtros() {
+
+export default function Filtros({ filtro, setFiltro }: Props) {
     function selecionaFiltro(opcao: IOpcao) {
-
+        if(filtro === opcao.id) return setFiltro(null);
+        return setFiltro(opcao.id);
     }
-    return(
+    return (
         <div className={styles.filtros}>
             {filtros.map((opcao) => (
-                <button 
-                    className={styles.filtros__filtro}
+                <button
+                    className={classNames({
+                        [styles.filtros__filtro]: true,
+                        [styles['filtros__filtro--ativo']]: filtro === opcao.id
+                    })}
                     key={opcao.id}
                     onClick={() => selecionaFiltro(opcao)}
                 >
-                    {opcao.label}
-                </button>
-            ))}
-        </div>
+            {opcao.label}
+        </button>
+    ))
+}
+        </div >
     );
 };
