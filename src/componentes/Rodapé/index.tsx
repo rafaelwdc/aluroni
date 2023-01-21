8b8d49b38d58286d9613fc9eeb280c35263c254a
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-20 22:36:00 -0300

    Adicionando fontes e normalize // React com TypeScript => React: lidando com arquivos estáticos

---
+++ src/componentes/Rodapé/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {ReactComponent as Logo} from "assets/logo.svg";
+
+export default function Rodape() {
+    return (
+        <footer>
+            <h2>Aluroni</h2>
+            <p>A melhor massa com o melhor atendimento, só aqui na Aluroni</p>
+            <Logo />
+        </footer>
+    )
+}
