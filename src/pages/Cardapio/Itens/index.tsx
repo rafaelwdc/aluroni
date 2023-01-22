ba160f75258a703d4ced807ff362866e8adbc371
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-22 19:33:40 -0300

    Criando os itens // React com TypeScript => React: lidando com arquivos estáticos

---
+++ src/pages/Cardapio/Itens/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import cardapio from './itens.json';
+import Item from './Item';
+import styles from './Itens.module.scss'
+
+export default function Itens() {
+    return (
+        <div className={styles.itens}>
+            {cardapio.map(item => (
+                <Item key={item.id} />
+            ))}
+        </div>
+    );
+};
