8862f01f6897a933a314cc77d384b357b8ee35ab
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-22 19:51:00 -0300

    Populando cada item // React com TypeScript => React: lidando com arquivos estáticos

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,30 +1,34 @@
-import React from 'react'
-import styles from './Item.module.scss'
-import logo from 'assets/logo.svg'
+import React from 'react';
+import styles from './Item.module.scss';
+import logo from 'assets/logo.svg';
+import cardapio from '../../Itens/itens.json';
 
-export default function Item() {
+type Props = typeof cardapio[0];
+
+export default function Item(props: Props) {
+    const { title, description, category, size, serving, price } = props;
     return (
         <div className={styles.item}>
             <div className={styles.item__imagem}>
-                <img src={logo} alt="Imagem" />
+                <img src={logo} alt={title} />
             </div>
             <div className={styles.item__descricao}>
                 <div className={styles.item__titulo}>
-                    <h2>Macarrão</h2>
-                    <p>Descrição do Macarrão</p>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
                 </div>
                 <div className={styles.item__tags}>
                     <div className={styles.item__tipo}>
-                        Massa
+                        {category.label}
                     </div>
                     <div className={styles.item__porcao}>
-                        400g
+                        {size}g
                     </div>
                     <div className={styles.item__qtdpessoas}>
-                        Serve 2 pessoas
+                        Serve {serving} pessoa{serving === 1 ? '' : 's'}
                     </div>
                     <div className={styles.item__valor}>
-                        R$ 50,00
+                        R$ {price.toFixed(2)}
                     </div>
                 </div>
             </div>
