d452ec7257571e73995b0a2a0f6306a7f2a5190f
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-24 04:48:01 -0300

    Reaproveitando o Menu // React com TypeScript => React: conhecendo a biblioteca React Router

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ReactComponent as Logo} from 'assets/logo.svg';
+import styles from './Menu.module.scss';
+
+export default function Menu() {
+    return (
+
+        <nav className={styles.menu}>
+            <Logo />
+        </nav>
+    );
+}
