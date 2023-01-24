716e6c0097a1e804fcdc5b7e80a6d30abde5d5c6
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-24 04:25:19 -0300

    Resolvendo rotas com o js // React com TypeScript => React: conhecendo a biblioteca React Router

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import Inicio from 'pages/Inicio';
+import Cardapio from 'pages/Cardapio';
+
+const componenteAtual = window.location.pathname === '/' ? <Inicio /> : <Cardapio />;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <Inicio />
+        {componenteAtual}
     </React.StrictMode>
 );
