9d2848c3bbe7f9341e66be0e4e32d706b37dd2db
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-24 07:08:04 -0300

    Terminando a tela de Início // React com TypeScript => React: conhecendo a biblioteca React Router

---
+++ src/pages/Sobre/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Sobre.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
+import React from 'react';
+
+export default function Sobre() {
+    return (
+        <section> Sobre </section>
+    );
+}
