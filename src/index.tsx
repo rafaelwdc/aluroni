1857619e8964d36a2552a3e7f6d70f7a264911b0
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-24 04:36:26 -0300

    Uso do React Router Dom // React com TypeScript => React: conhecendo a biblioteca React Router

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-import Inicio from 'pages/Inicio';
-import Cardapio from 'pages/Cardapio';
 
-const componenteAtual = window.location.pathname === '/' ? <Inicio /> : <Cardapio />;
+import Router from 'routes';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        {componenteAtual}
+        <Router />
     </React.StrictMode>
 );
