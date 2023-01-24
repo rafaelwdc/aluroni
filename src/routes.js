0d19ff5f19e0671df09ac0f91846fe7daf8071c9
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-24 06:57:25 -0300

    Estilo Tema // React com TypeScript => React: conhecendo a biblioteca React Router

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,21 @@
 import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 export default function AppRouter() {
-    return(
-        <Router>
-            <Menu />
-            <Routes>
-                <Route index path='/' element={<Inicio />} />
-                <Route path='/cardapio' element={<Cardapio />} />
-            </Routes>
-        </Router>
+    return (
+        <main>
+            <Router>
+                <Menu />
+                <Routes>
+                    <Route path='/' element={<PaginaPadrao />}>
+                        <Route index element={<Inicio />} />
+                        <Route path='cardapio' element={<Cardapio />} />
+                    </Route>
+                </Routes>
+            </Router>
+        </main>
     );
 }
