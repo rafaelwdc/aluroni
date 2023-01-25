eb47880f2692c32e1940dbe59431d90b0296a703
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-24 23:43:28 -0300

    Resolvendo o Header // React com TypeScript => React: conhecendo a biblioteca React Router

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
 import NotFound from 'pages/NotFound';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function Prato() {
     const { id } = useParams();
@@ -13,7 +14,7 @@ export default function Prato() {
     }
     
     return (
-        <>
+        <PaginaPadrao>
             <button 
                 className={styles.voltar}
                 onClick={() => navigate(-1)}
@@ -34,6 +35,6 @@ export default function Prato() {
                 </div>
                 <TagsPrato {...prato} />
             </section>
-        </>
+        </PaginaPadrao>
     );
 }
