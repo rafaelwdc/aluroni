78f63d110359b187e8f4b5d2dd3a6b04d7d8e481
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-24 23:21:34 -0300

    Refatorando tags // React com TypeScript => React: conhecendo a biblioteca React Router

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,8 +1,7 @@
 import styles from './Prato.module.scss';
 import { useParams, useNavigate } from 'react-router-dom';
-import React from 'react';
-import classNames from 'classnames';
 import cardapio from 'data/cardapio.json';
+import TagsPrato from 'components/TagsPrato';
 
 export default function Prato() {
     const { id } = useParams();
@@ -31,24 +30,8 @@ export default function Prato() {
                     <p className={styles.conteudo__descricao}>
                         {prato.description}
                     </p>
-                    <div className={styles.tags}>
-                        <div className={classNames({
-                            [styles.tags__tipo]: true,
-                            [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]: true
-                        })}>
-                            {prato.category.label}
-                        </div>
-                        <div className={styles.tags__porcao}>
-                            {prato.size}g
-                        </div>
-                        <div className={styles.tags__qtdpessoas}>
-                            Serve {prato.serving} pessoa{prato.serving === 1 ? '' : 's'}
-                        </div>
-                        <div className={styles.tags__valor}>
-                            R$ {prato.price.toFixed(2)}
-                        </div>
-                    </div>
                 </div>
+                <TagsPrato {...prato} />
             </section>
         </>
     );
