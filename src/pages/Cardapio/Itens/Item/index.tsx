5a86911f2c208491cd61c6e05187e69478687f3d
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-11 19:58:02 -0300

    Reconciliação - 1. Entendendo a estrutura => React: otimizando a performance

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -2,13 +2,14 @@ import styles from './Item.module.scss';
 import { Prato } from 'types/Prato';
 import TagsPrato from 'components/TagsPrato';
 import { useNavigate } from 'react-router-dom';
+import { memo } from 'react';
 
-export default function Item(props: Prato) {
+function Item(props: Prato) {
     const { id, title, description, photo } = props;
-    id === 1 && console.log(title);    
+    id === 1 && console.log(title);
     const navigate = useNavigate();
     return (
-        <div 
+        <div
             className={styles.item}
             onClick={() => navigate(`/prato/${id}`)}
         >
@@ -25,3 +26,4 @@ export default function Item(props: Prato) {
         </div>
     );
 }
+export default memo(Item);
